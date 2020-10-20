4669ea4614d3cdf7c478750e3d4fe981b925940c
Corete <fanthell95@gmail.com>
2020-10-20 14:05:26 +0900

    세팅 수정

--- src/components/Nav/Login.js
+++ src/components/Nav/Login.js
@@ -2,52 +2,8 @@ import React, { Component } from "react";
 import "./Login.scss";
 
 class Login extends Component {
-  state = {
-    email: "",
-    passWord: ""
-  };
-
   render() {
-    return (
-      <>
-        {this.props.open ? (
-          <div className="modal">
-            <div onClick={this.props.close}>
-              <div className="login_modal">
-                <div className="modalContents" onClick={this.props.open}>
-                  <img className="login_logo" src="watcha.png" />
-                  <h2>로그인</h2>
-                  <div className="login_input">
-                    <input
-                      className="login_id"
-                      type="text"
-                      placeholder="이메일"
-                    />
-                    <input
-                      className="login_pw"
-                      type="password"
-                      placeholder="비밀번호"
-                    />
-                  </div>
-                  <div className="login_button">
-                    <button>로그인</button>
-                  </div>
-                  <div className="pw_button">
-                    <button>비밀번호를 잊어버리셨나요?</button>
-                  </div>
-                  <div className="signup">
-                    계정이 없으신가요?
-                    <button>회원가입</button>
-                  </div>
-                  <hr className="or"></hr>
-                  <button className="facebook_btn">Facebook 으로 로그인</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : null}
-      </>
-    );
+    return <></>;
   }
 }
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,30 +3,8 @@ import "./Main.scss";
 import Login from "../../components/Nav/Login";
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isModalOpen: false
-    };
-  }
-
-  openModal = () => {
-    this.setState({ isModalOpen: true });
-  };
-
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
-  render() {
-    return (
-      <>
-        <button onClick={this.openModal}>Check</button>
-        <button onClick={this.openModal}>signup</button>
-
-        <Login open={this.state.isModalOpen} close={this.closeModal} />
-      </>
-    );
-  }
+  <>
+  </>
 }
 
 export default Main;
