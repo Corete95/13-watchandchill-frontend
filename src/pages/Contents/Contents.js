a4dc7dbdc8c374e3542c47a9d68f0b443f5433c8
JIAN <jian7061@gmail.com>
2020-10-22 14:26:45 +0900

    보고싶어요 클릭시 코멘트 창 보여주기 완료

--- src/pages/Contents/Contents.js
+++ src/pages/Contents/Contents.js
@@ -23,6 +23,7 @@ class Contents extends Component {
 
           <div className="movie_title_section">
             <img
+              className="movie_title_section-poster"
               src="https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1467960957/ywqqu9uz55x8ysn4jupi.jpg"
               alt="인생은 아름다워 poster"
             ></img>
@@ -31,31 +32,34 @@ class Contents extends Component {
               <h1>인생은 아름다워</h1>
               <div>1997 · 드라마 · 이탈리아</div>
               <div>
-                <span>예상 ★3.6 </span>
-                <span>· 평균 ★4.4(43만명)</span>
+                <span>평균 ★4.4(43만명)</span>
               </div>
-              <button
-                onClick={() => {
-                  this.setState({ clicked: true });
-                }}
-                className={
-                  this.state.clicked ? "addBtn-clicked" : "addBtn-not_clicked"
-                }
-              >
-                <FontAwesomeIcon icon={faPlus} className="plus_icon" />
-                &nbsp;&nbsp;보고싶어요
-                {/* <img src={pinkFlag}></img> */}
-              </button>
-              <button
-                className={
-                  this.state.clicked
-                    ? "dropdownBtn-clicked"
-                    : "dropdownBtn-not_clicked"
-                }
-              >
-                <CaretDownOutlined />
-              </button>
-
+              <div className="buttons">
+                <button
+                  onClick={() => {
+                    this.setState({ clicked: true });
+                  }}
+                  className={
+                    this.state.clicked ? "addBtn-clicked" : "addBtn-not_clicked"
+                  }
+                >
+                  {this.state.clicked ? (
+                    <img src={pinkFlag} className="pinkFlag" />
+                  ) : (
+                    <FontAwesomeIcon icon={faPlus} className="plus_icon" />
+                  )}
+                  &nbsp;&nbsp;보고싶어요
+                </button>
+                <button
+                  className={
+                    this.state.clicked
+                      ? "dropdownBtn-clicked"
+                      : "dropdownBtn-not_clicked"
+                  }
+                >
+                  <CaretDownOutlined  className="dropdown_icon"/>
+                </button>
+              </div>
               {/* <span className="rating">평가하기</span> */}
             </div>
           </div>
