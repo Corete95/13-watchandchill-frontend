90c4b2402d254dd1905059fea1fa4576b7e5bef9
Corete <fanthell95@gmail.com>
2020-10-24 22:25:16 +0900

    프로필 페이지 레이아웃 완성

--- src/components/Nav/profile.js
+++ src/components/Nav/profile.js
@@ -8,7 +8,40 @@ class profile extends Component {
         <div className="profile">
           <div className="profile_top">
             <div className="profile_margin">
-              <div className="profie_box">asdasd</div>
+              <div className="profie_box">
+                <div className="box_first">
+                  <buton className="box_top_gear"></buton>
+                </div>
+                <div className="box_second">
+                  <div className="box_center">
+                    <div className="box_center_profile">
+                      <div className="profile_circle">
+                        <div className="circle"></div>
+                      </div>
+                      <h1>김정현</h1>
+                      <div className="text">프로필이 없습니다.</div>
+                    </div>
+                    <div className="taste_analysis">
+                      <a title="taste_analysis">
+                        <span className="icon"></span>
+                        <sapn className="text">취향분석</sapn>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="box_bottom">
+                    <a>
+                      <ul>
+                        <li>영화</li>
+                        <li>★5</li>
+                        <li className="imissyou">
+                          보고싶어요
+                          <strong>2</strong>
+                        </li>
+                      </ul>
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
