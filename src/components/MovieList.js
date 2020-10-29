023c68b3fc9b19abf2d1cd61592c090a720b6f9a
Jo Hye Mi <dev.summerr@gmail.com>
2020-10-29 16:16:14 +0900

    Modify : routing 경로 변경

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import "./MovieList.scss";
 
 class MovieList extends Component {
   render() {
-    console.log(`this.props.title=>${this.props.title}`);
-    console.log(`this.props.id=>${this.props.id}`);
+    // console.log(`this.props.title=>${this.props.title}`);
+    // console.log(`this.props.id=>${this.props.id}`);
     return (
-      <div className="MovieList">
-        <Link to={`/contents/${this.props.id}`}>
-          <div className="movieImg">
-            <div className="moviePoster">
-              <img className="posterImg" src={this.props.poster} alt="poster" />
-              <div className="movieTitle">{this.props.title}</div>
-              <div className="openingDate">{this.props.date}</div>
-              <div className="country">{this.props.country}</div>
-              <div className="genre">{this.props.genre}</div>
-            </div>
+      <div
+        className="MovieList"
+        onClick={() => this.props.history.push(`/contents/${this.props.id}`)}
+      >
+        <div className="movieImg">
+          <div className="moviePoster">
+            <img className="posterImg" src={this.props.poster} alt="poster" />
+            <div className="movieTitle">{this.props.title}</div>
+            <div className="openingDate">{this.props.date}</div>
+            <div className="country">{this.props.country}</div>
+            <div className="genre">{this.props.genre}</div>
           </div>
-        </Link>
+        </div>
       </div>
     );
   }
 }
 
-export default MovieList;
+export default withRouter(MovieList);
